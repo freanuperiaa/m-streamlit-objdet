78d8b8724ae195445d09901ae13854994022760d
Lorelorelore <malore.delacuesta@gmail.com>
2022-05-14 15:27:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,27 +42,27 @@ st.set_page_config(page_title="Object Detection", page_icon="🧿")
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [
+        # {
+        #     "urls": ["stun:stun.l.google.com:19302"]
+        # },
         {
-            "urls": ["stun.nextcloud.com:443"]
+            "urls": ["stun:openrelay.metered.ca:80"],
         },
-        # {
-        #     "urls": ["stun:openrelay.metered.ca:80"],
-        # },
-        # {
-        #     "urls": ["turn:openrelay.metered.ca:80"],
-        #     "username": "openrelayproject",
-        #     "credential": "openrelayproject",
-        # },
-        # {
-        #     "urls": ["turn:openrelay.metered.ca:443"],
-        #     "username": "openrelayproject",
-        #     "credential": "openrelayproject",
-        # },
-        # {
-        #     "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
-        #     "username": "openrelayproject",
-        #     "credential": "openrelayproject",
-        # },
+        {
+            "urls": ["turn:openrelay.metered.ca:80"],
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": ["turn:openrelay.metered.ca:443"],
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
     ]},
     media_stream_constraints={
         "video": True,
