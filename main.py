5ea44ba45c9719b2937f15fe3a8a6609b751e1d5
Lorelorelore <malore.delacuesta@gmail.com>
2022-05-14 15:45:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,28 +42,7 @@ st.set_page_config(page_title="Object Detection", page_icon="🧿")
 
 WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [
-        # {
-        #     "urls": ["stun:stun.l.google.com:19302"]
-        # },
-        {
-            "urls": ["stun:openrelay.metered.ca:80"],
-        },
-        {
-            "urls": ["turn:openrelay.metered.ca:80"],
-            "username": "openrelayproject",
-            "credential": "openrelayproject",
-        },
-        {
-            "urls": ["turn:openrelay.metered.ca:443"],
-            "username": "openrelayproject",
-            "credential": "openrelayproject",
-        },
-        {
-            "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
-            "username": "openrelayproject",
-            "credential": "openrelayproject",
-        },
-    ]},
+        {"urls": ["stun:stun.l.google.com:19302"]}]},
     media_stream_constraints={
         "video": True,
     },
